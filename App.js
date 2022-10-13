58cdd79449530902856776edf994c5c39b1c296a
hervis30 <hervisjonat@gmail.com>
2022-10-12 20:34:32 -0500

    se configura place holder

--- App.js
+++ App.js
@@ -1,14 +1,120 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Button, View, TextInput } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useState } from 'react';
 
-export default function App() {
+function UserScreen({ navigation }) {
+  const [fullname, setFullname] = useState('');
+  const [email, setEmail] = useState('');
+
+  const validate = () => {
+    if (email == "tv@gmail.com") {
+      setEmail("");
+      setFullname("")
+      navigation.navigate('Profile', { fullname: fullname })
+    }
+  }
   return (
-    <View style={styles.container}>
-      <Text>practica</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <TextInput
+        style={styles.inputs}
+        placeholder={'Nombre completo'}
+        onChangeText={value => setFullname(value)}
+        value={fullname}
+      />
+      <TextInput
+        style={styles.inputs}
+        placeholder={'Correo electronico'}
+        onChangeText={value => setEmail(value)}
+        value={email}
+      />
+      <Button
+        title="Perfil del Usuario"
+        //onPress={() => navigation.navigate('Settings')}
+        //onPress={validate}
+        onPress={() => {
+          if (email == "tv@gmail.com") {
+            setEmail("");
+            setFullname("")
+            navigation.navigate('Profile', { fullname: fullname })
+          }
+        }}
+
+      />
     </View>
   );
 }
+
+function ProfileScreen({ route }) {
+  return (
+    <View style={styles.container}>
+      <Text>Perfil: {route.params.fullname}</Text>
+    </View>
+  );
+}
+
+function AccountScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Cuentas</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Configuración</Text>
+      <Button
+        title="Perfil"
+        onPress={() => navigation.navigate('Feed')}
+      />
+    </View>
+  );
+}
+
+function LoginScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Configuración</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+function HomeTabs() {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false
+      }}
+    >
+      {/* tabBarStyle: desactiva el menú bottom */}
+      <Tab.Screen name="User" component={UserScreen} options={{
+        tabBarStyle: { display: "none" }
+      }} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+      <Tab.Screen name="Account" component={AccountScreen} />
+
+    </Tab.Navigator>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeTabs} options={{ title: 'Sistema Bancario' }} />
+        <Stack.Screen name="Settings" component={SettingsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +123,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  inputs: {
+    borderWidth: 1,
+    borderColor: 'green',
+    borderRadius: 10,
+    padding: 10,
+    textAlign: 'center',
+    marginBottom: 5
+  }
 });
